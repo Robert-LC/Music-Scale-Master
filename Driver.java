68c73c2861bfb5bf0021c05149a7da31ac9e45a4
Robert Locicero <robertlocicerojr@gmail.com>
2020-11-05 18:50:41 -0500

    Set up basic framework for the Scale Class

--- Driver.java
+++ Driver.java
@@ -1,10 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays; // Use for ToString Method
+
 
 public class Driver 
 {
+
+    String scaleList[] = {"A Major", "A Minor", "A# Major", "A# Minor", "B Major", "B Minor", "C Major", "C Minor",
+        "C# Major", "C# Major", "D Major", "D# Minor", "E Major", "E Minor", "F Major", "F Minor", "F# Major", "F# Minor",
+        "G Major", "G Minor", "G# Major", "G# Minor"};
     public static void main(String[] args) 
     {
         
+        
+        
+        System.exit(0);
     }
 }
--- Scale.java
+++ Scale.java
@@ -1,4 +1,66 @@
+import javax.swing.JOptionPane;
+
+
 public class Scale 
 {
-    String scaleList[] = {};
+    private String name;
+    private String[] notes;
+    private String[] chordSequence;
+    private int noteCount;
+
+    public Scale(String name, String[] notes, String[] chordSequence, int noteCount)
+    {
+        setName(name);
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setNotes(String[] notes)
+    {
+        while(notes.length > 7 && notes.length < 5)
+        {
+            JOptionPane.showMessageDialog(null, "Please enter a scale with 7 or 5 notes.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        this.notes = notes;
+    }
+
+    public void setChordSequence(String[] chordSequence)
+    {
+        while(chordSequence.length > 7 && chordSequence.length < 5)
+        {
+            JOptionPane.showMessageDialog(null, "Please enter a Chord Sequence with 7 or 5 notes.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        this.chordSequence = chordSequence;
+    }
+
+    public void setNoteCount(int noteCount)
+    {
+        this.noteCount = noteCount;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String[] getNotes()
+    {
+        return notes;
+    }
+
+    public String[] getChordSequence()
+    {
+        return chordSequence;
+    }
+
+    public int getNoteCount()
+    {
+        return noteCount;
+    }
+
 }
